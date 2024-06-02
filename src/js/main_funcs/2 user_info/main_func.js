b3a98863ef076fb2c4ce920b197a8a905fdea741
Matestampa <paredpiso5@gmail.com>
2024-06-02 03:52:58 -0300

    Added use of getData shared button

--- src/js/main_funcs/2 user_info/main_func.js
+++ src/js/main_funcs/2 user_info/main_func.js
@@ -2,14 +2,21 @@ import {get_Manager} from "../../app.js";
 
 import { get_userInfo } from "../../get_data/user_info.js";
 
+import { get_sharedButton } from "../ui_shared.js";
+
 
 async function main_get_userInfo(){
     //Obtener username
     let username=document.getElementById('inputUsername').value;
     
+    let shared_button=get_sharedButton();
+    
     //Traer data
+    shared_button.disable();
+    
     let data=await get_userInfo(username);
     
+    shared_button.enable();
     //Si no hubo errors, y tenemos la data
     if (data){
         
--- src/js/main_funcs/2 user_info/ui.js
+++ src/js/main_funcs/2 user_info/ui.js
@@ -1,3 +1,12 @@
-function setUI_userInfo(){}
+import {create_sharedButton} from "../ui_shared.js";
+import { main_get_userInfo } from "./main_func.js";
+
+function setUI_userInfo(){      
+      let HTML=`<button id="getData_button">user_info</button>`
+      document.getElementById("userInfo_mainDiv").innerHTML=HTML;
+
+      let shared_button=create_sharedButton(document.getElementById("getData_button"));
+      shared_button.change_callback(main_get_userInfo);
+}
 
 export {setUI_userInfo};
